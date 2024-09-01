baed26cfe8b8ee724e9a427f10ed945bc190cee4
Mark Ophir <markophir@gmail.com>
2024-09-01 12:53:33 +0300

    Updated Flask app configuration and set to run on port 5001

--- weather_app.py
+++ weather_app.py
@@ -83,4 +83,4 @@ def future_weather_route():
         return jsonify({"error": "Weather data for the given date is not available."}), 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
